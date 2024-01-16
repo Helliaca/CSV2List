de6bc5d540c3701882760daca1184c0d695cf640
Benjamin <Helliaca@users.noreply.github.com>
2024-01-16 12:42:27 +0100

    base files

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # CSV2List
 Simple, lightweight C# library to read and write Lists of objects to CSV files.
+
+(C# 2.1+)
+
+## Usage
+
+C# Datacontainer class:
+
+```chsarp
+public class MyDataContainer
+{
+    [CSV2List.CSVField("id")]
+    public int id;
+
+    [CSV2List.CSVField("name")]
+    public string name;
+}
+```
+
+CSV file:
+
+```
+id;name
+1;robert
+52;sarah
+32;philip
+```
+
+Reading/Writing:
+
+```csharp
+List<MyDataContainer> myList = CSVReader.ReadFromCSV<MyDataContainer>("my_file.csv");
+
+myList[1].id = 42;
+
+CSVWriter.WriteToCSV(myList, "output/my_corrected_file.csv");
+```
---
+++ src/CSV2List.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CSV2List
+{
+    [System.AttributeUsage(System.AttributeTargets.Field, AllowMultiple = false)]
+    public class CSVField : System.Attribute
+    {
+        public string FieldName;
+
+        public CSVField(string FieldName) {
+            this.FieldName = FieldName;
+        }
+    }
+
+
+    public static class CSVReader
+    {
+        public static List<T> ReadFromCSV<T>(string filepath, char delimeter = ';') where T : new()
+        {
+            // Create return list
+            var result = new List<T>();
+
+            // Read file contents
+            var lines = File.ReadAllLines(filepath);
+
+            if (lines.Length < 2)
+            {
+                throw new Exception("Tried to read a CSV with no content!");
+            }
+
+            // Get headers
+            var headers = lines[0].Split(delimeter);
+
+            // Read data
+            for (int i = 1; i < lines.Length; i++)
+            {
+                // Create new object of type T
+                var obj = new T();
+
+                // Get values of the current CSV entry
+                var values = lines[i].Split(delimeter);
+
+                // Get all the properties
+                foreach (var prop in typeof(T).GetFields())
+                {
+                    // If they have a CSVField property
+                    var attribute = prop.GetCustomAttribute<CSVField>();
+                    if (attribute != null)
+                    {
+                        var index = Array.IndexOf(headers, attribute.FieldName);
+                        if (index != -1)
+                        {
+                            var value = Convert.ChangeType(values[index], prop.FieldType);
+                            prop.SetValue(obj, value);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Tried to set object from CSV. Object has a field {attribute.FieldName}, but the CSV file does not contain it! Ignoring...");
+                        }
+                    }
+                }
+
+                result.Add(obj);
+            }
+
+            return result;
+        }
+    }
+
+    public static class CSVWriter { 
+
+        public static void WriteToCSV<T>(List<T> list, string filepath, char delimeter=';') where T : new()
+        {
+            var sb = new StringBuilder();
+
+            // Get properties with CSVField attribute
+            var properties = typeof(T).GetFields()
+                .Where(prop => Attribute.IsDefined(prop, typeof(CSVField)));
+
+            // Write headers
+            sb.AppendLine(string.Join(delimeter, properties.Select(prop => ((CSVField)Attribute.GetCustomAttribute(prop, typeof(CSVField))).FieldName)));
+
+            // Write data
+            foreach (var obj in list)
+            {
+                sb.AppendLine(string.Join(delimeter, properties.Select(prop => prop.GetValue(obj))));
+            }
+
+            // Ensure directory exists
+            var directory = Path.GetDirectoryName(filepath);
+            Directory.CreateDirectory(directory);
+
+            File.WriteAllText(filepath, sb.ToString());
+        }
+    }
+}
